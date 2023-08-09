db2117f6a4f820af9ad442ccdf5dfc45c8ba0ffa
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-08-09 19:20:35 +0200

    task 15 fix typo

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
     new_str = ''
-    for i, c in enamurate(str):
+    for i, c in enumerate(str):
         if not i == n:
             new_str += c
     return new_str
