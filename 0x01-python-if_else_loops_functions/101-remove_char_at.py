a053f35ef05385f45943a5e11638de3f04d892c6
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-08-09 19:19:44 +0200

    task 15

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    return str[:n-1] + str[n+1:]
+    new_str = ''
+    for i, c in enamurate(str):
+        if not i == n:
+            new_str += c
+    return new_str
