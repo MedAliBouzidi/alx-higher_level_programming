075f65be5126de8c9cd90d9f43c034512c740648
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-08-10 22:50:46 +0200

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     counter = len(sys.argv) - 1
     if counter == 0:
         print("0 arguments.")
-    if counter == 1:
+    elif counter == 1:
         print("1 argument:")
     else:
         print("{} arguments:".format(counter))
