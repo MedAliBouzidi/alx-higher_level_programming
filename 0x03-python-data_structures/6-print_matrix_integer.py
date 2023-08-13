307ddfa73ee7fc9a1af213bc5428d1d564e00eb8
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-08-13 17:49:56 +0200

    task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,4 @@
 def print_matrix_integer(matrix=[[]]):
     if isinstance(matrix, list):
         for row in matrix:
-            for cell in row:
-                print("{:d}".format(cell), end=" ")
-            print(end="\n")
+            print(" ".join("{:d}".format(i) for i in row))
