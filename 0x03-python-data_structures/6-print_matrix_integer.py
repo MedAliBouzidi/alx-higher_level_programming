9b009df1685e6f1138f30867f4c23ed85556c618
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-08-13 17:44:48 +0200

    task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,6 @@
 #!/usr/bin/pyhton3
 def print_matrix_integer(matrix=[[]]):
-    trans = []
     for row in matrix:
-        trans_row = []
-        for i in row:
-            trans_row.append(i)
-        trans.append(trans_row)
-    for row in trans:
-        print("{:d}".format(i) for i in row)
+        for cell in row:
+            print("{:d}".format(cell), end=" ")
+        print(end="\n")
