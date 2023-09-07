db5776aadec7358cd725a39b7d4409b85db63e57
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-09-07 12:32:31 +0200

    task 3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    total = 0
+    count = len(sys.argv) - 1
+    for index in range(count):
+        total += int(sys.argv[index + 1])
+    print("{}".format(total))
