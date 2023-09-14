b609b25c3006355b040cbe81814bf631179d3696
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-09-14 18:00:55 +0200

    updaet task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python4
+#!/usr/bin/python333
 def search_replace(my_list, search, replace):
     if isinstance(my_list, list):
         return list(map(lambda x: replace if x == search else x, my_list))
