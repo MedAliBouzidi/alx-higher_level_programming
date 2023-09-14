fbd8cde2534b63db85898a14efeef7c531a33725
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-09-14 19:03:57 +0200

    update task 14

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return list(map(lambda mat: list(map(lambda elem: elem * elem, mat)), matrix))
+    return list(map(lambda m: list(map(lambda el: el * el, m)), matrix))
