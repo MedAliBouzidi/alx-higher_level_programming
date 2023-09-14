b0e1402eb2e0641d31863e14ba73ab648c5dcf9d
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-09-14 17:59:49 +0200

    update task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = []
-    for mat in matrix:
-        new_matrix.append(list(map(lambda x: x * x, m)))
-    return new_matrix
+    return list(list(map(lambda x: x * x, m)) for m in matrix)
