63db16e585cb2aa20b41d1268d23f5353d72fedd
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-09-25 15:43:55 +0200

    update task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
 	if isinstance(my_list, list):
-		try:
-			for idx in range(x + 1):
+		for idx in range(x + 1):
+			try:
 				print(my_list[idx], end='')
-		except:
-			pass
-		print(end="\n")
+			except:
+				break
+		print("")
 		return idx
