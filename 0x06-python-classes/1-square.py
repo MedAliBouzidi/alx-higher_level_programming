53bc5c188acadf9741531c380b56516640b8b7f6
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-09-26 18:17:26 +0200

    update task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-""" Define a class Square """
 
 
 class Square:
-	""" Class Squre """
-	def __init__(self, size):
-		self.__size = size
+    """
+    class Square with private instance attribute size
+    """
+    def __init__(self, size):
+        self.__size = size
