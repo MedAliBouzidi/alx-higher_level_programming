89f586b138a6e588473e7350f2902a5ce8df092e
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-09-27 16:17:08 +0200

    update task 7

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -4,14 +4,14 @@
 
 class Node:
     """ Class Node """
-    def __init__(self,data, next_node=None):
+    def __init__(self, data, next_node=None):
         self.data = data
         self.next_node = next_node
 
     @property
     def data(self):
         return (self.__data)
-    
+
     @data.setter
     def data(self, value):
         if (not isinstance(value, int)):
@@ -21,22 +21,22 @@ class Node:
     @property
     def next_node(self):
         return (self.__next_node)
-    
+
     @next_node.setter
     def next_node(self, value):
         if (not isinstance(value, Node) and value is not None):
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
-    
+
 
 class SinglyLinkedList:
     """ Class SinglyLinkedList """
     def __init__(self):
         self.__head = None
 
-    def sorted_insert(self ,value):
+    def sorted_insert(self, value):
         new = Node(value)
-        if self.__head == None:
+        if self.__head is None:
             new.next_node = None
             self.__head = new
         elif self.__head.data > value:
