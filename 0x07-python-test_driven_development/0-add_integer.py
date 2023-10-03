a04bdd97097cf33b66779de4557c0fd5f7118c57
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-10-03 14:20:41 +0200

    task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,21 +1,18 @@
-#!usr/bin/python3
-""" Add Integers """
+#!/usr/bin/python3
 def add_integer(a, b=98):
-    """
-    Function for adding two numbers
+    """ Function for adding two numbers
 
-    Args:
-        a: first number
-        b: second number
+    Retunr a + b
 
-    Returns:
-        The sum of a and b
-    
+    a and b must be integers or floats
+
     Raises:
-        TypeError: If a or b aren't intege rand/or float
+        TypeError: if they are not floar or integers
+
     """
-    if not isinstance(a, int) and not isinstance(a, float):
-        raise TypeError("a must be an integer")
-    if not isinstance(b, int) and not isinstance(b, float):
-        raise TypeError("b must be an integer")
+
+    if ((not isinstance(a, int) and not isinstance(a, float))):
+        raise TypeError('a must be an integer')
+    if ((not isinstance(b, int) and not isinstance(b, float))):
+        raise TypeError('b must be an integer')
     return (int(a) + int(b))
