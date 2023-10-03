3722d256af0c31eeb0d2cdd830c28425b9811ae5
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-10-03 15:15:22 +0200

    task 29

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
     magic_string.occ = getattr(magic_string, 'occ', 0) + 1
-    return (f"{'BestSchool, ' * (magic_string.occ - 1)} BestSchool")
+    return ("BestSchool, " * (magic_string.occ - 1) + "BestSchool")
