b477297267b88c2525c74275ebbe2718848432e8
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-10-09 17:58:42 +0200

    task 7

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Define a new class"""
+
+
+class BaseGeometry:
+    """ init a new class """
+
+    def area(self):
+        """Raise error"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
