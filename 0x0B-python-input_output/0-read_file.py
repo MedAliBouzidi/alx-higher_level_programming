a6319ef4f3599aac28003e1ce7041b7c708d7546
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-10-10 18:44:10 +0200

    task 1

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" unction that reads a text file (UTF8) and prints it to stdout """
+""" function that reads a text file (UTF8) and prints it to stdout """
 
 
 def read_file(filename=""):
