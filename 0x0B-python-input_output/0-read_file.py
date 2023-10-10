c2f63cca7cfa152230e99d5e348397d1f7802103
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-10-10 18:37:39 +0200

    upadte task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,5 @@
 
 def read_file(filename=""):
     with open(filename, encoding="UTF8") as f:
-        lines = f.readlines()
-        for line in lines:
-            print(line, end='')
+        date = f.read()
+        print(date, end='')
