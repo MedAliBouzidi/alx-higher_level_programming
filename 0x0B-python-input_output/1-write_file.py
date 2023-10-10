5d510ee5eb35d841ef5f31603e562cd99bc7e013
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-10-10 18:50:08 +0200

    task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,6 +6,5 @@
 
 
 def write_file(filename="", text=""):
-    with open(filename, 'w+', encoding='utf-8') as f:
-        f.write(text)
-    return len(text)
+    with open(filename, 'w', encoding='utf-8') as f:
+        return f.write(text)
