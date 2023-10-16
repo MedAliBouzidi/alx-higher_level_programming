586ecb1165768f2a5f4309be0079c10c2437deb7
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-10-17 01:23:29 +0200

    task 12

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -21,3 +21,36 @@ class Square(Rectangle):
     def __str__(self):
         return "[Square] ({:d}) {:d}/{:d} - {:d}"\
             .format(self.id, self.x, self.y, self.size)
+
+    def update(self, *args, **kwargs):
+        """ Update the Square """
+
+        if args and len(args) != 0:
+            idx = 0
+            for arg in args:
+                if idx == 0:
+                    if arg is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif idx == 1:
+                    self.size = arg
+                elif idx == 2:
+                    self.x = arg
+                elif idx == 3:
+                    self.y = arg
+                idx += 1
+
+        elif kwargs and len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == "id":
+                    if value is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = value
+                elif key == "size":
+                    self.size = value
+                elif key == "x":
+                    self.x = value
+                elif key == "y":
+                    self.y = value
