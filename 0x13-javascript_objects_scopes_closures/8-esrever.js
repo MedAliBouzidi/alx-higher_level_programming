cdd7838a8ed61c0d06960f671ef5e06cf76ecc6c
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-12-12 22:35:37 +0100

    task 8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+exports.esrever = function (list) {
+  const reversed = [];
+  for (let index = list.length - 1; index >= 0; index--) {
+    reversed.push(list[index]);
+  }
+  return reversed;
+};
