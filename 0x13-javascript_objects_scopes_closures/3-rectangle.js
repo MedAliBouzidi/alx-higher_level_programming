5b07ca3125e7b543f55cfdbb26c4829505a2d009
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-12-12 22:10:21 +0100

    task 3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,1 +1,22 @@
 #!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    const rect = [];
+    for (let row = 0; row < this.height; row++) {
+      for (let col = 0; col < this.width; col++) {
+        rect.push('X');
+      }
+      if (row !== this.height - 1) {
+        rect.push('\n');
+      }
+    }
+    console.log(rect.join(''));
+  }
+};
