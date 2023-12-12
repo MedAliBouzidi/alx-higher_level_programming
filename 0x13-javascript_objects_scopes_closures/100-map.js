bf5c72edb61821bde2b2eb7fc51362fb1f6c9a8f
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-12-12 23:01:18 +0100

    task 11

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,1 +1,6 @@
 #!/usr/bin/node
+const oldList = Array.from(require('./100-data.js').list);
+const newList = oldList.map((element, index) => element * index);
+
+console.log(oldList);
+console.log(newList);
