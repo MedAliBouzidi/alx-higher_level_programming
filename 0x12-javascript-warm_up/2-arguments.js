d2719881de2133d1a384183f8815d146fb03ee2c
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-12-12 02:00:02 +0100

    update task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 const argc = process.argv.length;
-(argc === 2) ? console.log('No Argument') : (argc === 3) ? console.log('Argument found') : console.log('Arguments found');
+(argc === 2) ? console.log('No argument') : (argc === 3) ? console.log('Argument found') : console.log('Arguments found');
