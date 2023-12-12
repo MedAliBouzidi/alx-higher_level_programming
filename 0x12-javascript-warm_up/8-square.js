aa675e570dab88a2bddf57f810491a9eda79afe2
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-12-12 16:18:07 +0100

    task 8 fix

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,13 +4,14 @@ const square = [];
 
 if (isNaN(size)) {
   console.log('Missing size');
-}
-for (let i = 0; i < size; i++) {
-  for (let j = 0; j < size; j++) {
-    square.push('X');
+} else {
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+      square.push('X');
+    }
+    if (i !== size - 1) {
+      square.push('\n');
+    }
   }
-  if (i !== size - 1) {
-    square.push('\n');
-  }
+  console.log(square.join(''));
 }
-console.log(square.join(''));
