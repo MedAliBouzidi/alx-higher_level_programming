8f962f08ff506812cc055440923b5fc3df394bc5
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-12-17 00:03:59 +0100

    udpate code

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,9 +5,12 @@ import MySQLdb
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print('<Usage>: ./0-select_states.py [mysql_username] [mysql_password] [datebase name]')
+        print('<Usage>: ./0-select_states.py\
+            [mysql_username] [mysql_password] [datebase name]')
 
-    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    conn = MySQLdb.connect(host="localhost", port=3306,
+                           user=sys.argv[1], passwd=sys.argv[2],
+                           db=sys.argv[3], charset="utf8")
     cur = conn.cursor()
     cur.execute("SELECT * FROM states ORDER BY id")
     rows = cur.fetchall()
