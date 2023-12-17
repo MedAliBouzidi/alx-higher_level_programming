016c52ab1ed8ab7035b58e5315c3d31226a7065a
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-12-17 15:33:24 +0100

    task 4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+"""
+    lists all cities from the database hbtn_0e_4_usa
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print('<Usage>: ./4-cities_by_state.py\
+            [mysql_username] [mysql_password] [database name]')
+
+    conn = MySQLdb.connect(host="localhost", port=3306,
+                           user=sys.argv[1], passwd=sys.argv[2],
+                           db=sys.argv[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT c.id, c.name, s.name \
+                FROM cities AS c, states AS s \
+                WHERE c.state_id = s.id \
+                ORDER BY c.id")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    conn.close()
