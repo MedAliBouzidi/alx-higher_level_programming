108cadb5d630527f1abf7eef7805d45b582b5091
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-12-17 15:46:12 +0100

    task 5

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -15,8 +15,8 @@ if __name__ == "__main__":
                            db=sys.argv[3], charset="utf8")
     cur = conn.cursor()
     cur.execute("SELECT c.id, c.name, s.name \
-                FROM cities AS c, states AS s \
-                WHERE c.state_id = s.id \
+                FROM cities AS c INNER JOIN states AS s \
+                ON s.id = c.state_id \
                 ORDER BY c.id")
     rows = cur.fetchall()
     for row in rows:
--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,1 +1,29 @@
 #!/usr/bin/python3
+"""
+    takes in the name of a state as an argument
+    and lists all cities of that state, using
+    the database hbtn_0e_4_usa
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        print('<Usage>: ./5-filter_cities.py\
+            [mysql_username] [mysql_password] [database name]\
+            [state name]')
+
+    conn = MySQLdb.connect(host="localhost", port=3306,
+                           user=sys.argv[1], passwd=sys.argv[2],
+                           db=sys.argv[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT c.id, c.name\
+                FROM cities AS c INNER JOIN states AS s\
+                ON s.id=c.state_id and s.name=%s ORDER BY id", (sys.argv[4], ))
+    rows = cur.fetchall()
+    res = []
+    for row in rows:
+        res.append(row[1])
+    print(', '.join(res))
+    cur.close()
+    conn.close()
