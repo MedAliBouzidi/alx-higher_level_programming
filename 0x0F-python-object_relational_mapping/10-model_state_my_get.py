78f8fb00e9b898775b9c4798941978e1d4c3d7fc
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-12-17 16:31:06 +0100

    task 10

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+"""
+     lists all State objects that contain
+     the letter a from the database hbtn_0e_6_usa
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        print('<Usage>: ./10-model_state_my_get.py\
+            [mysql_username] [mysql_password] [database name]\
+             [state name]')
+    else:
+        engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                               .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+        Base.metadata.create_all(engine)
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        instance = session.query(State) \
+            .filter(State.name == (sys.argv[4],)).first()
+        try:
+            print(instance.id)
+        except:
+            print('Not found')
