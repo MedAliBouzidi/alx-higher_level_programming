c8a2566338db5ff2ab599e1dc5336409600f1aa5
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-12-17 14:31:23 +0100

    update task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -9,13 +9,13 @@ import MySQLdb
 if __name__ == "__main__":
     if len(sys.argv) != 4:
         print('<Usage>: ./0-select_states.py\
-            [mysql_username] [mysql_password] [datebase name]')
+            [mysql_username] [mysql_password] [database name]')
 
     conn = MySQLdb.connect(host="localhost", port=3306,
                            user=sys.argv[1], passwd=sys.argv[2],
                            db=sys.argv[3], charset="utf8")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id")
     rows = cur.fetchall()
     for row in rows:
         print(row)
